726d5380e093cb6355e3326e2aa3000050e63adc
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-21 10:10:37 +0100

    Feat: Show all

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles=Article.all
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
