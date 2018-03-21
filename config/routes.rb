5505e6402fa2478e25a56143a87d7be51a97d306
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-21 09:16:11 +0100

    Feat: Paint a view creted by myself

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :articles
+ 
+  
   # get 'welcome/index'
-  get 'welcome/index'
-
+  root 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
