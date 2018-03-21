925fff5c17ae55f170cc19ef28aa8c02219975cd
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-21 13:00:45 +0100

    Feat: Delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,6 +28,28 @@ class ArticlesController < ApplicationController
     @articles=Article.all
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+   
+    # @article.update(title: 'A new title')
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
+  end
+
   private
   def article_params
 
